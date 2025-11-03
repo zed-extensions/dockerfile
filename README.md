1c9350dcbbe717d983a41b0e955e22de46284fc1
Remy Suen <remy.suen@gmail.com>
2025-11-03 05:19:09 -0500

    Integrate with the Buildx Dockerfile debugger (#27)
    Buildx implements DAP to allow users to debug Dockerfile builds.
    
    <img width="1666" height="842" alt="image"
    src="https://github.com/user-attachments/assets/9cda3d58-6d04-48b4-a204-9009a6264908"
    />
    
    <img width="3106" height="1319" alt="image"
    src="https://github.com/user-attachments/assets/88092f52-d251-4090-acca-0792f01fbe58"
    />
    
    ---------
    
    Signed-off-by: Remy Suen <remy.suen@docker.com>

--- README.md
+++ README.md
@@ -10,7 +10,34 @@ To support matching filenames other than `Dockerfile` you can add [`file_types`]
 ```json
 {
   "file_types": {
-    "Dockerfile": [ "Dockerfile.*" ]
+    "Dockerfile": ["Dockerfile.*"]
   }
 }
 ```
+
+## Debugging
+
+The extension supports debugging Dockerfile builds with [Buildx](https://github.com/docker/buildx). The minimal required version of Buildx is v0.28.0. To get Buildx, we recommend installing or updating [Docker Desktop](https://docs.docker.com/install/). You may alternatively install Buildx manually by following the instructions [here](https://github.com/docker/buildx?tab=readme-ov-file#manual-download).
+
+You can validate your Buildx installation by running `BUILDX_EXPERIMENTAL=1 docker buildx dap`.
+
+You can create a debug configuration by modifying your project's `.zed/debug.json`.
+
+```json
+{
+  "label": "Docker: Build", // required, configurable
+  "adapter": "buildx-dockerfile", // required, must not be modified
+  "request": "launch", // required, must not be modified
+  "contextPath": "/home/username/worktree", // optional, defaults to ${ZED_WORKTREE_ROOT}
+  "dockerfile": "/home/username/worktree/Dockerfile", // optional, defaults to ${ZED_WORKTREE_ROOT}/Dockerfile
+  "target": "test", // optional, should be a build stage in the Dockerfile
+  "stopOnEntry": true, // if the debugger should suspend on the first line, defaults to false
+  "args": [
+    // additional arguments for the build command
+    "--build-arg",
+    "NODE_ENV=development"
+  ]
+}
+```
+
+While a build has been suspended, you can evaluate `exec` to open a shell into the Docker image that has been built up to that point in time.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+use std::path::Path;
 use std::{env, fs};
 use zed_extension_api::{self as zed, Result};
 
@@ -6,6 +8,24 @@ const PACKAGE_NAME: &str = "dockerfile-language-server-nodejs";
 
 struct DockerfileExtension {
     did_find_server: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct DockerfileDebugConfig {
+    // The absolute path to the context
+    context_path: Option<String>,
+    // The absolute path to the Dockerfile being built
+    dockerfile: Option<String>,
+    // This should only ever be "launch" as "attach" is unsupported
+    request: String,
+    // args for the build, such as --build-arg ...
+    #[serde(default)]
+    args: Vec<String>,
+    // Should the debugger suspend immediately on the first line
+    stop_on_entry: Option<bool>,
+    // The build stage to build
+    target: Option<String>,
 }
 
 impl DockerfileExtension {
@@ -78,6 +98,122 @@ impl zed::Extension for DockerfileExtension {
                 "--stdio".to_string(),
             ],
             env: Default::default(),
+        })
+    }
+
+    fn dap_request_kind(
+        &mut self,
+        adapter_name: String,
+        value: zed::serde_json::Value,
+    ) -> Result<zed::StartDebuggingRequestArgumentsRequest, String> {
+        if adapter_name != "buildx-dockerfile" {
+            return Err(format!(
+                "Unexpected debug adapter launched in the Dockerfile extension \"{adapter_name}\""
+            ));
+        }
+
+        value
+            .get("request")
+            .and_then(|request| {
+                request.as_str().and_then(|s| match s {
+                    "launch" => Some(zed::StartDebuggingRequestArgumentsRequest::Launch),
+                    "attach" => None,
+                    _ => None,
+                })
+            })
+            .ok_or_else(|| {
+                "Invalid request, expected `request` to be `launch`, `attach` or any other value is unsupported".into()
+            })
+    }
+
+    fn dap_config_to_scenario(
+        &mut self,
+        zed_scenario: zed::DebugConfig,
+    ) -> Result<zed::DebugScenario, String> {
+        match zed_scenario.request {
+            zed::DebugRequest::Launch(launch) => {
+                let config = DockerfileDebugConfig {
+                    dockerfile: Some(launch.program),
+                    context_path: launch.cwd,
+                    request: "launch".to_string(),
+                    args: launch.args,
+                    stop_on_entry: zed_scenario.stop_on_entry,
+                    target: None,
+                };
+
+                let config = zed::serde_json::to_value(config)
+                    .map_err(|e| e.to_string())?
+                    .to_string();
+
+                Ok(zed::DebugScenario {
+                    adapter: zed_scenario.adapter,
+                    label: zed_scenario.label,
+                    config,
+                    tcp_connection: None,
+                    build: None,
+                })
+            }
+            zed::DebugRequest::Attach(_) => {
+                return Err("attaching to a running build is not supported".to_string());
+            }
+        }
+    }
+
+    fn get_dap_binary(
+        &mut self,
+        adapter_name: String,
+        config: zed::DebugTaskDefinition,
+        _user_provided_debug_adapter_path: Option<String>,
+        worktree: &zed::Worktree,
+    ) -> zed_extension_api::Result<zed::DebugAdapterBinary, String> {
+        if adapter_name != "buildx-dockerfile" {
+            return Err(format!(
+                "Unexpected debug adapter launched in the Dockerfile extension \"{adapter_name}\""
+            ));
+        }
+
+        let configuration: serde_json::Value = serde_json::from_str(&config.config)
+            .map_err(|e| format!("`config` is not a valid JSON: {e}"))?;
+        let mut dockerfile_config: DockerfileDebugConfig =
+            serde_json::from_value(configuration.clone())
+                .map_err(|e| format!("`config` is not a valid Dockerfile config: {e}"))?;
+
+        // Inject defaults if not set
+        let root_path_str = worktree.root_path();
+        if dockerfile_config.context_path.is_none() {
+            dockerfile_config.context_path = Some(root_path_str.clone());
+        }
+        if dockerfile_config.dockerfile.is_none() {
+            let root_path = Path::new(&root_path_str);
+            let dockerfile_path = root_path.join("Dockerfile");
+            let dockerfile_str = dockerfile_path
+                .to_str()
+                .ok_or_else(|| {
+                    format!(
+                        "Dockerfile path contains invalid UTF-8: {:?}",
+                        dockerfile_path
+                    )
+                })?
+                .to_owned();
+            dockerfile_config.dockerfile = Some(dockerfile_str);
+        }
+
+        let final_config = serde_json::to_string(&dockerfile_config)
+            .map_err(|e| format!("Failed to serialize config: {e}"))?;
+
+        let mut arguments = vec!["buildx".to_string(), "dap".to_string(), "build".to_string()];
+        arguments.extend(dockerfile_config.args);
+
+        Ok(zed::DebugAdapterBinary {
+            command: Some("docker".to_string()),
+            arguments,
+            cwd: Some(worktree.root_path()),
+            envs: vec![(String::from("BUILDX_EXPERIMENTAL"), String::from("1"))],
+            request_args: zed::StartDebuggingRequestArguments {
+                request: zed::StartDebuggingRequestArgumentsRequest::Launch,
+                configuration: final_config,
+            },
+            connection: None,
         })
     }
 }
